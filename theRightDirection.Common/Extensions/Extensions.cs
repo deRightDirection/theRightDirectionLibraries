a7a76b5c20f1e67119f0826ada47a2333cc18a27
Mannus Etten <github@therightdirection.nl>
2018-10-30 10:15:12 +0100

    nieuwe libraries toegevoegd

---
+++ theRightDirection.Common/Extensions/Extensions.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theRightDirection.Extensions
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExcludeFromCopyPropertyAttribute : Attribute
+    {
+    }
+    public class PropertyInfoComparer : IEqualityComparer<PropertyInfo>
+    {
+        public bool Equals(PropertyInfo x, PropertyInfo y)
+        {
+            return x.Name.ToLowerInvariant().Equals(y.Name.ToLowerInvariant());
+        }
+
+        public int GetHashCode(PropertyInfo obj)
+        {
+            return obj.Name.GetHashCode();
+        }
+    }
+
+    /// <summary>
+    /// extension method voor een object om een deep copy te maken van een object
+    /// </summary>
+    public static partial class Extensions
+    {
+        public static void CopyProperties(this object from, object to, bool includePropertiesFromBaseType = false, string[] excludedProperties = null)
+        {
+            if (to == null)
+            {
+                return;
+            }
+            Dictionary<string, PropertyInfo> sourceProperties = GetProperties(from.GetType(), includePropertiesFromBaseType);
+            Dictionary<string, PropertyInfo> targetProperties = GetProperties(to.GetType(), includePropertiesFromBaseType);
+            IEnumerable<string> commonPropertiesName = sourceProperties.Values.Intersect(targetProperties.Values, new PropertyInfoComparer()).Select(x => x.Name);
+            foreach (var commonPropertyName in commonPropertiesName)
+            {
+                if (excludedProperties != null
+                  && excludedProperties.Contains(commonPropertyName))
+                    continue;
+                var hasExcludeAttribute = FindAttribute(targetProperties[commonPropertyName]);
+                if (!hasExcludeAttribute)
+                {
+                    var value = sourceProperties[commonPropertyName].GetValue(from, null);
+                    if (targetProperties[commonPropertyName].CanWrite)
+                    {
+                        targetProperties[commonPropertyName].SetValue(to, value, null);
+                    }
+                }
+            }
+        }
+
+        public static int GetHashCodeOnProperties(this object inspect)
+        {
+            return inspect.GetType().GetTypeInfo().DeclaredProperties.Select(o => o.GetValue(inspect)).GetListHashCode();
+        }
+
+        public static int GetListHashCode(this IEnumerable<object> sequence)
+        {
+            return sequence.Where(x => x != null)
+                .Select(item => item.GetHashCode())
+                .Aggregate((total, nextCode) => total ^ nextCode);
+        }
+
+        private static bool FindAttribute(PropertyInfo commonProperty)
+        {
+            var attribute = commonProperty.GetCustomAttribute<ExcludeFromCopyPropertyAttribute>();
+            return attribute != null;
+        }
+
+        private static Dictionary<string, PropertyInfo> GetProperties(Type type, bool recursive)
+        {
+            if (recursive)
+            {
+                Dictionary<string, PropertyInfo> properties = new Dictionary<string, PropertyInfo>();
+                var retrievedProperties = GetProperties(type);
+                foreach(var retrievedProperty in retrievedProperties)
+                {
+                    properties.Add(retrievedProperty.Key, retrievedProperty.Value);
+                }
+                var typeInfo = type.GetTypeInfo();
+                while (typeInfo.BaseType != null)
+                {
+                    var baseType = typeInfo.BaseType;
+                    retrievedProperties = GetProperties(baseType);
+                    foreach (var retrievedProperty in retrievedProperties)
+                    {
+                        if (!properties.ContainsKey(retrievedProperty.Key))
+                        {
+                            properties.Add(retrievedProperty.Key, retrievedProperty.Value);
+                        }
+                    }
+                    typeInfo = baseType.GetTypeInfo();
+                }
+                return properties;
+            }
+            return GetProperties(type);
+        }
+
+        private static Dictionary<string, PropertyInfo> GetProperties(Type type)
+        {
+            var typeInfo = type.GetTypeInfo();
+            Dictionary<string, PropertyInfo> properties = new Dictionary<string, PropertyInfo>();
+            foreach (var property in typeInfo.DeclaredProperties)
+            {
+                properties.Add(property.Name, property);
+            }
+            return properties;
+        }
+    }
+}
---
+++ theRightDirection.WPF.Xaml/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace theRightDirection.WPF.Xaml.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return Visibility.Visible;
+            }
+            else
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = (Visibility)value;
+            return visibility == Visibility.Visible ? true : false;
+        }
+    }
+}
