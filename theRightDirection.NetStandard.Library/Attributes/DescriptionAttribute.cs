7f3837d7929e6c0acfce031f7609cac4ca662032
Mannus <info@mannus.nl>
2018-11-04 19:23:35 +0100

    kleine aanpassingen

---
+++ theRightDirection.NetStandard.Library/Attributes/DescriptionAttribute.cs
@@ -0,0 +1,27 @@
+﻿namespace theRightDirection.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// Defines an attribute for providing a string based description.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All)]
+    public class DescriptionAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DescriptionAttribute"/> class.
+        /// </summary>
+        /// <param name="description">
+        /// The description.
+        /// </param>
+        public DescriptionAttribute(string description)
+        {
+            this.Description = description;
+        }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ theRightDirection.NetStandard.Library/Extensions/Extensions.Object.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using theRightDirection.Attributes;
+using theRightDirection.Comparers;
+
+namespace theRightDirection
+{
+    public static partial class Extensions
+    {
+        public static void CopyProperties(this object from, object to, bool includePropertiesFromBaseType = false, string[] excludedProperties = null)
+        {
+            if (to == null)
+            {
+                return;
+            }
+            IEnumerable<PropertyInfo> sourceProperties = GetProperties(from.GetType(), includePropertiesFromBaseType);
+            IEnumerable<PropertyInfo> targetProperties = GetProperties(to.GetType(), includePropertiesFromBaseType);
+            IEnumerable<PropertyInfo> commonProperties = sourceProperties.Intersect(targetProperties, new PropertyInfoComparer());
+            foreach (var commonProperty in commonProperties)
+            {
+                if (excludedProperties != null
+                  && excludedProperties.Contains(commonProperty.Name))
+                    continue;
+                var hasExcludeAttribute = FindAttribute(commonProperty);
+                if (!hasExcludeAttribute)
+                {
+                    var value = commonProperty.GetValue(from, null);
+                    if (commonProperty.CanWrite)
+                    {
+                        commonProperty.SetValue(to, value, null);
+                    }
+                }
+            }
+        }
+
+        public static int GetHashCodeOnProperties(this object inspect)
+        {
+            return inspect.GetType().GetTypeInfo().DeclaredProperties.Select(o => o.GetValue(inspect)).GetListHashCode();
+        }
+
+        public static int GetListHashCode(this IEnumerable<object> sequence)
+        {
+            return sequence.Where(x => x != null)
+                .Select(item => item.GetHashCode())
+                .Aggregate((total, nextCode) => total ^ nextCode);
+        }
+
+        private static bool FindAttribute(PropertyInfo commonProperty)
+        {
+            var attribute = commonProperty.GetCustomAttribute<ExcludeFromCopyPropertyAttribute>();
+            return attribute != null;
+        }
+
+        private static IEnumerable<PropertyInfo> GetProperties(Type type, bool recursive)
+        {
+            if (recursive)
+            {
+                List<PropertyInfo> properties = new List<PropertyInfo>();
+                properties.AddRange(GetProperties(type));
+                var typeInfo = type.GetTypeInfo();
+                while (typeInfo.BaseType != null)
+                {
+                    var baseType = typeInfo.BaseType;
+                    properties.AddRange(GetProperties(baseType));
+                    typeInfo = baseType.GetTypeInfo();
+                }
+                return properties;
+            }
+            return GetProperties(type);
+        }
+
+        private static IEnumerable<PropertyInfo> GetProperties(Type type)
+        {
+            var typeInfo = type.GetTypeInfo();
+            return typeInfo.DeclaredProperties;
+        }
+    }
+}
