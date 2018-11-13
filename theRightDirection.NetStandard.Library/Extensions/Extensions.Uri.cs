735617389d84f445a51bde9d3c3e3ecaddd7c300
Mannus <info@mannus.nl>
2018-11-13 22:50:26 +0100

    nieuwe uri extension

---
+++ theRightDirection.NetStandard.Library/Extensions/Extensions.Uri.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace theRightDirection.Library.Extensions
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// Adds or Updates the specified parameter to the Query String.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="paramName">Name of the parameter to add.</param>
+        /// <param name="paramValue">Value for the parameter to add.</param>
+        /// <returns>Url with added parameter.</returns>
+        public static Uri AddOrUpdateParameter(this Uri url, string paramName, string paramValue)
+        {
+            var uriBuilder = new UriBuilder(url);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+
+            if (query.AllKeys.Contains(paramName))
+            {
+                query[paramName] = paramValue;
+            }
+            else
+            {
+                query.Add(paramName, paramValue);
+            }
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.Uri;
+        }
+    }
+}
