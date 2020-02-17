d4bfa1fcd3cd9df2a0263b93a31a227a5952c19a
the Right Direction <github@therightdirection.nl>
2020-02-17 19:43:18 +0100

    update en grid verwijderd

--- theRightDirection.WPF.Xaml/Converters/EnumDisplayEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace theRightDirection.WPF.Xaml.Converters
-{
-    public class EnumDisplayEntry
-    {
-        public string EnumValue { get; set; }
-        public string DisplayString { get; set; }
-        public bool ExcludeFromDisplay { get; set; }
-    }
-}
