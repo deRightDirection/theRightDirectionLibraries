8c4584e7c66d042104504ce3902c181282d8fbd8
the Right Direction <github@therightdirection.nl>
2020-04-11 12:06:15 +0200

    nieuwe assert gemaakt

---
+++ theRightDirection.NetCore.Library.UnitTesting/theRightDirection.NetCore.Library.UnitTesting/Assert.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace theRightDirection.Library.UnitTesting.NetCore
+{
+
+    public sealed class ExceptionAssertionExtensions
+    {
+        public static void NoExceptionThrown<T>(Action a) where T : Exception
+        {
+            try
+            {
+                a();
+            }
+            catch (T)
+            {
+                Assert.Fail("Expected no {0} to be thrown", typeof(T).Name);
+            }
+        }
+    }
+}
