f0d8fcb701d4d1e13f64efc95f576d16f654c8fe
the Right Direction <mannus.etten@ibm.com>
2020-10-22 15:57:17 +0200

    nieuwe extension method voor dispatcher

---
+++ theRightDirection.Library/Extensions/Extensions.Dispatcher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace theRightDirection.Library
+{
+    public static partial class Extensions
+    {
+        public static void InvokeIfRequired(this Dispatcher dispatcher, Action action)
+        {
+            if (dispatcher == null)
+            {
+                return;
+            }
+            if (!dispatcher.CheckAccess())
+            {
+                dispatcher.BeginInvoke(action, DispatcherPriority.ContextIdle);
+                return;
+            }
+            action();
+        }
+    }
+}
