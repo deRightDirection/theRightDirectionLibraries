51836b591bdc2746c3a9a8bf203c064a7227d816
the Right Direction <github@therightdirection.nl>
2020-11-25 07:45:27 +0100

    aanpassing iwebclient

--- theRightDirection.NetStandard.Library/IWebClient.cs
+++ theRightDirection.NetStandard.Library/IWebClient.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Text;
 
 namespace theRightDirection.Library
 {
-    public interface IWebClient
+    public interface IWebClient : IDisposable
     {
         bool UseDefaultCredentials { get; set; }
         string DownloadString(string url);
+
+        NameValueCollection QueryString { get; set; }
+        byte[] UploadValues(string address, string method, NameValueCollection data);
     }
 }
