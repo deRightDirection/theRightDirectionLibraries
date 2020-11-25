17caa591f7873df99381e4efa4e6e27e8e42212f
the Right Direction <github@therightdirection.nl>
2020-11-25 07:25:03 +0100

    test interface voor webclient

---
+++ theRightDirection.NetStandard.Library/IWebClient.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace theRightDirection.Library
+{
+    public interface IWebClient
+    {
+        bool UseDefaultCredentials { get; set; }
+        string DownloadString(string url);
+    }
+}
