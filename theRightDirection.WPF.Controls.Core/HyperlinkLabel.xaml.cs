8ed67aa638b6a4a5941a02c75b8254d1a94fec36
the Right Direction <github@therightdirection.nl>
2020-12-14 08:31:57 +0100

    .net 5 controls library

---
+++ theRightDirection.WPF.Controls.Core/HyperlinkLabel.xaml.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace theRightDirection.WPF.Controls
+{
+    public partial class HyperlinkLabel : UserControl
+    {
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string),
+            typeof(HyperlinkLabel), new PropertyMetadata(default(string)));
+
+        public static readonly DependencyProperty UrlProperty = DependencyProperty.Register("Url", typeof(Uri),
+            typeof(HyperlinkLabel), new PropertyMetadata(default(Uri)));
+
+        public HyperlinkLabel()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public Uri Url
+        {
+            get { return (Uri)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(e.Uri.AbsoluteUri);
+            }
+            catch
+            {
+                // hack because of this: https://github.com/dotnet/corefx/issues/10361
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    Process.Start(new ProcessStartInfo("cmd", $"/c start {e.Uri}") { CreateNoWindow = true });
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", e.Uri.AbsoluteUri);
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    Process.Start("open", e.Uri.AbsoluteUri);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
