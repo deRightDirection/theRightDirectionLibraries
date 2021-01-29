6fdc23c915dd652162f84fee4c0c9e5a3bb2963a
the Right Direction <github@therightdirection.nl>
2021-01-29 20:51:29 +0100

    datetime extension

---
+++ theRightDirection.Library.NetCore/Extensions/Extensions.DateTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace theRightDirection
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// convert a unix timestamp with milliseconds to a datetime-object
+        /// </summary>
+        public static DateTime TimeStampAsDateTime(this double unixTimeStamp)
+        {
+            var converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return converted.AddMilliseconds(unixTimeStamp);
+        }
+
+        /// <summary>
+        /// convert a unix timestamp with milliseconds to a datetime-object
+        /// </summary>
+        public static DateTime TimeStampAsDateTime(this long unixTimeStamp)
+        {
+            var converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return converted.AddMilliseconds(unixTimeStamp);
+        }
+
+        public static DateTime TimeStampAsDateTime(this DateTime dateTime, double unixTimeStamp)
+        {
+            DateTime converted = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            DateTime newDateTime = converted.AddMilliseconds(unixTimeStamp);
+            return newDateTime;
+        }
+    }
+}
