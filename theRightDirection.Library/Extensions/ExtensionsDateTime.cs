60cfaae25aa00dc446dd1deee841d3e7b4cb5de3
the Right Direction <github@therightdirection.nl>
2021-02-25 21:26:06 +0100

    oude extensie method

--- theRightDirection.Library/Extensions/ExtensionsDateTime.cs
+++ theRightDirection.Library/Extensions/ExtensionsDateTime.cs
@@ -8,5 +8,11 @@ namespace theRightDirection.Library
 {
     public static partial class Extensions
     {
+        public static double TimeStampInMilliseconds(this DateTime dateTime)
+        {
+            var utcTime = dateTime.ToUniversalTime();
+            var t = (utcTime - new DateTime(1970, 1, 1));
+            return Math.Truncate(t.TotalMilliseconds);
+        }
     }
 }
