acb61d50bcf411dfca68e2b3d70886bfe1d757bf
Mannus Etten <info@therightdirection.nl>
2021-09-26 17:03:13 +0200

    kleine aanpassing

--- theRightDirection.Library.NetCore/Extensions/Extensions.Long.cs
+++ theRightDirection.Library.NetCore/Extensions/Extensions.Long.cs
@@ -13,18 +13,19 @@ namespace theRightDirection
         /// </summary>
         /// <param name="fileLength"></param>
         /// <returns></returns>
-        public static string ToFileLengthRepresentation(this long fileLength)
+        public static string ToFileLengthRepresentation(this long fileLength, bool spaceBetweenNumberAndAbbreviation = true)
         {
+            var spacer = spaceBetweenNumberAndAbbreviation ? " " : string.Empty;
             if (fileLength >= 1 << 30)
-                return $"{fileLength >> 30}Gb";
+                return $"{fileLength >> 30}{spacer}Gb";
 
             if (fileLength >= 1 << 20)
-                return $"{fileLength >> 20}Mb";
+                return $"{fileLength >> 20}{spacer}Mb";
 
             if (fileLength >= 1 << 10)
-                return $"{fileLength >> 10}Kb";
+                return $"{fileLength >> 10}{spacer}Kb";
 
-            return $"{fileLength}B";
+            return $"{fileLength}{spacer}B";
         }
     }
 }
