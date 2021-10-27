da9e20aa5b51f7f15f9ff00129da12b0293ddc22
Mannus Etten <info@therightdirection.nl>
2021-10-27 20:50:17 +0200

    nieuwe method en class urihelper

---
+++ theRightDirection.Library.NetCore/UriHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theRightDirection
+{
+    public class UriHelper
+    {
+        /// <summary>
+        /// create an uri from address and port
+        /// </summary>
+        public static Uri CreateFromAddressAndPort(string address, int port)
+        {
+            try
+            {
+                var uriBuilder = new UriBuilder(address) { Port = port };
+                return uriBuilder.Uri;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
