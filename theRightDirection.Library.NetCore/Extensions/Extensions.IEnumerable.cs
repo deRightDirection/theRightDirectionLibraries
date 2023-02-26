bb6de68ab8b5231c90d38bd3674195ccd04603e5
Mannus Etten <info@therightdirection.nl>
2023-02-26 10:38:50 +0100

    nieuwe release met dictionary met guard en string extensies

---
+++ theRightDirection.Library.NetCore/Extensions/Extensions.IEnumerable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theRightDirection
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// converteer een ienumerable naar een dictionary met controle
+        /// of keys al bestaan in de dictionary. Indien dat het geval is wordt
+        /// er een logmelding weggeschreven
+        /// </summary>
+        /// <typeparam name="T">type voor de key</typeparam>
+        /// <typeparam name="U">type voor de value</typeparam>
+        /// <typeparam name="V">type van de lijst</typeparam>
+        /// <param name="list">de lijst om te converteren naar een dictionary</param>
+        /// <param name="key">de eigenschap van een object gebruikt mag worden als key</param>
+        /// <param name="property">de eigenschap van een object gebruikt mag worden als value</param>
+        /// <returns>dictionary van type T,U</returns>
+        public static Dictionary<T, U> ToDictionaryWithSafeGuard<T, U, V>(this IEnumerable<V> list, Func<V, T> key, Func<V, U> property)
+        {
+            var result = new Dictionary<T, U>();
+            list.ForEach(x =>
+            {
+                var keyValue = key(x);
+                var value = property(x);
+                if (!result.ContainsKey(keyValue))
+                {
+                    result.Add(keyValue, value);
+                }
+            });
+            return result;
+        }
+    }
+}
---
+++ theRightDirection.Library.UnitTests/IEnuerableExtensionsTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoBogus;
+using Xunit;
+
+namespace theRightDirection.Library.UnitTests
+{
+    public class IEnuerableExtensionsTest
+    {
+        [Fact]
+        public void ToDictionaryWithSafeGuard1()
+        {
+            var items = AutoFaker.Generate<ArcGISItem>(5);
+            var dictionary = items.ToDictionaryWithSafeGuard(x => x.EntityId, x => x.Owner);
+            dictionary.Count.Should().Be(items.Count);
+        }
+
+        [Fact]
+        public void ToDictionaryWithSafeGuard2()
+        {
+            var items = AutoFaker.Generate<ArcGISItem>(5);
+            var firstItem = items.First();
+            items.Add(firstItem);
+            var dictionary = items.ToDictionaryWithSafeGuard(x => x.EntityId, x => x.Owner);
+            dictionary.Count.Should().Be(items.Count - 1);
+        }
+        [Fact]
+        public void ToDictionaryWithoutSafeguard()
+        {
+            var items = AutoFaker.Generate<ArcGISItem>(5);
+            var firstItem = items.First();
+            items.Add(firstItem);
+            Action act = () => items.ToDictionary(x => x.EntityId, x => x.Owner);
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+
+    public class ArcGISItem
+    {
+        public string EntityId { get; set; }
+        public string Owner { get; set; }
+    }
+}
