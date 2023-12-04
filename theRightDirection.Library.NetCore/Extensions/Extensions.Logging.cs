117f5251da77b64df8ceda29c3ebda08362df0a3
Mannus Etten <info@therightdirection.nl>
2023-12-04 12:30:30 +0100

    extensie voor serilog toegevoegd

--- theRightDirection.Library.NetCore/Extensions/Extensions.Logging.cs
+++ theRightDirection.Library.NetCore/Extensions/Extensions.Logging.cs
@@ -5,59 +5,58 @@ using System.Configuration;
 using System.Diagnostics;
 using System.Reflection;
 
-namespace theRightDirection
-{
-    public static partial class Extensions
+namespace theRightDirection;
+
+public static partial class Extensions
+{ 
+    public static void Exception(this ILog logger, Exception exception)
     {
-        public static void Exception(this ILog logger, Exception exception)
-        {
-            logger.Error(exception.Message, exception);
-        }
+        logger.Error(exception.Message, exception);
+    }
 
-        public static void LogEnvironmentInformation(this ILog logger)
-        {
-            LogEnvironmentInformation(logger, new List<string>()); ;
-        }
+    public static void LogEnvironmentInformation(this ILog logger)
+    {
+        LogEnvironmentInformation(logger, new List<string>()); ;
+    }
 
-        public static void LogEnvironmentInformation(this ILog logger, List<string> additionalLinesWithInformation)
+    public static void LogEnvironmentInformation(this ILog logger, List<string> additionalLinesWithInformation)
+    {
+        logger.Info("----- environment configuration -----");
+        logger.Info(Assembly.GetEntryAssembly().DirectoryOfAssembly());
+        var osInformation = new SystemInformationHelper();
+        //            logger.Info($"{osInformation.WindowsVersionName}{Environment.NewLine}{osInformation.Architecture}{Environment.NewLine}build {osInformation.BuildNumber}");
+        foreach (var lineWithInformation in additionalLinesWithInformation)
         {
-            logger.Info("----- environment configuration -----");
-            logger.Info(Assembly.GetEntryAssembly().DirectoryOfAssembly());
-            var osInformation = new SystemInformationHelper();
-            //            logger.Info($"{osInformation.WindowsVersionName}{Environment.NewLine}{osInformation.Architecture}{Environment.NewLine}build {osInformation.BuildNumber}");
-            foreach (var lineWithInformation in additionalLinesWithInformation)
+            logger.Info(lineWithInformation);
+        }
+        logger.Info("-------------------------------------");
+    }
+
+    public static void LogApplicationSettings(this ILog logger)
+    {
+        logger.Info("----- application configuration -----");
+        var applicationSettings = ConfigurationManager.AppSettings;
+        if (applicationSettings != null)
+        {
+            foreach (string k in applicationSettings.Keys)
             {
-                logger.Info(lineWithInformation);
+                logger.Info($"{k}={applicationSettings[k]}");
             }
-            logger.Info("-------------------------------------");
         }
+        logger.Info("-------------------------------------");
+    }
 
-        public static void LogApplicationSettings(this ILog logger)
+    public static string GetStackTrace(this ILog logger)
+    {
+        var stackTrace = new StackTrace();
+        var stackFrames = stackTrace.GetFrames();
+        if (stackFrames.Length < 4)
         {
-            logger.Info("----- application configuration -----");
-            var applicationSettings = ConfigurationManager.AppSettings;
-            if (applicationSettings != null)
-            {
-                foreach (string k in applicationSettings.Keys)
-                {
-                    logger.Info($"{k}={applicationSettings[k]}");
-                }
-            }
-            logger.Info("-------------------------------------");
+            return string.Empty;
         }
-
-        public static string GetStackTrace(this ILog logger)
-        {
-            var stackTrace = new StackTrace();
-            var stackFrames = stackTrace.GetFrames();
-            if (stackFrames.Length < 4)
-            {
-                return string.Empty;
-            }
-            var trace = stackFrames[3];
-            var mb = trace.GetMethod();
-            var t = mb.DeclaringType;
-            return $"{t}.{mb.Name}";
-        }
+        var trace = stackFrames[3];
+        var mb = trace.GetMethod();
+        var t = mb.DeclaringType;
+        return $"{t}.{mb.Name}";
     }
 }
---
+++ theRightDirection.Library.NetCore/Extensions/Extensions.Serilog.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+using Serilog;
+
+namespace theRightDirection;
+public static partial class Extensions
+{
+    public static ILogger Here(this ILogger logger, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    {
+        var y = Path.GetFileNameWithoutExtension(sourceFilePath);
+        return logger.ForContext("MethodInformation", $"{y}.{memberName}:{sourceLineNumber}");
+    }
+}
