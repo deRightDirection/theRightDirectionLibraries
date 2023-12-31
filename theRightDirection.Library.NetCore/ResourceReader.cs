9d69e01fa8b6720f8c532f970cebb64a613b2813
Mannus Etten <info@therightdirection.nl>
2023-12-31 11:21:41 +0100

    kleine update voor de resource reader

--- theRightDirection.Library.NetCore/ResourceReader.cs
+++ theRightDirection.Library.NetCore/ResourceReader.cs
@@ -1,10 +1,58 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace theRightDirection;
 
 public class ResourceReader
 {
+    /// <summary>
+    /// get the list of resources in the folder, resource files are not allowed to have multiple .-character in the name except for the extension
+    /// </summary>
+    public IEnumerable<string> GetResources(string subFolder = "")
+    {
+        var assembly = Assembly.GetCallingAssembly();
+        var resources = assembly.GetManifestResourceNames();
+        var result = new List<string>();
+        var module = assembly.ManifestModule.Name.Replace(".dll", string.Empty);
+        resources.ForEach(x =>
+        {
+            var resourceWithoutPrefix = x.Replace($"{module}.", string.Empty);
+            var add = CanFileBeAdded(resourceWithoutPrefix, subFolder);
+            if (add)
+            {
+                var fileNameToAdd = subFolder.HasText()
+                    ? resourceWithoutPrefix.Replace($"{subFolder}.", string.Empty)
+                    : resourceWithoutPrefix;
+                result.Add(fileNameToAdd);
+            }
+        });
+        return result;
+    }
+
+    private static bool CanFileBeAdded(string fileName, string subFolder)
+    {
+        var splitParts = fileName.Split('.');
+        if (subFolder.HasNoText())
+        {
+            if (splitParts.Length == 2)
+            {
+                return true;
+            }
+        }
+        else
+        {
+            if (splitParts.Length > 2 && splitParts[0].Equals(subFolder, StringComparison.CurrentCultureIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public string ReadDataFromResource(string resourceName, string subFolder = "")
     {
         var assembly = Assembly.GetCallingAssembly();
--- theRightDirection.Library.UnitTests/ResourceReaderTest.cs
+++ theRightDirection.Library.UnitTests/ResourceReaderTest.cs
@@ -1,37 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using FluentAssertions;
 using Xunit;
-using static System.Net.Mime.MediaTypeNames;
 
-namespace theRightDirection.Library.UnitTests
+namespace theRightDirection.Library.UnitTests;
+
+public class ResourceReaderTest
 {
-    public class ResourceReaderTest
+    private readonly ResourceReader _resourceReader;
+
+    public ResourceReaderTest()
     {
-        [Fact]
-        public void Logo_Is_Found_As_Resource()
-        {
-            var resourceReader = new ResourceReader();
-            var image = resourceReader.ReadDataFromResourceAsFile("tRD-logo-1regel.png");
-            image.Length.Should().BeGreaterOrEqualTo(10);
-        }
-        [Fact]
-        public void Text_Is_Read_From_Resource()
-        {
-            var resourceReader = new ResourceReader();
-            var text = resourceReader.ReadDataFromResource("test.txt", "TestResources");
-            text.Should().Be("hallo!");
-        }
-        [Fact]
-        public void Text_In_Bytes_From_Resource()
-        {
-            var resourceReader = new ResourceReader();
-            var text = resourceReader.ReadDataFromResourceAsFile("test.txt", "TestResources");
-            text.Length.Should().BeGreaterOrEqualTo(9);
-        }
+        _resourceReader = new ResourceReader();
+    }
+
+    [Fact]
+    public void GetResources_No_Folder()
+    {
+        var resources = _resourceReader.GetResources();
+        resources.Count().Should().Be(1);
+        var data = _resourceReader.ReadDataFromResourceAsFile(resources.First());
+        data.Length.Should().BeGreaterThanOrEqualTo(9);
+    }
+    [Fact]
+    public void GetResources_With_Folder()
+    {
+        var resources = _resourceReader.GetResources("TestResources");
+        resources.Count().Should().Be(1);
+        var data = _resourceReader.ReadDataFromResourceAsFile(resources.First(), "TestResources");
+        data.Length.Should().BeGreaterThanOrEqualTo(9);
+    }
+
+    [Fact]
+    public void Logo_Is_Found_As_Resource()
+    {
+        var image = _resourceReader.ReadDataFromResourceAsFile("tRD-logo-1regel.png");
+        image.Length.Should().BeGreaterOrEqualTo(10);
+    }
+    [Fact]
+    public void Text_Is_Read_From_Resource()
+    {
+        var text = _resourceReader.ReadDataFromResource("test.txt", "TestResources");
+        text.Should().Be("hallo!");
+    }
+    [Fact]
+    public void Text_In_Bytes_From_Resource()
+    {
+        var text = _resourceReader.ReadDataFromResourceAsFile("test.txt", "TestResources");
+        text.Length.Should().BeGreaterOrEqualTo(9);
     }
 }
