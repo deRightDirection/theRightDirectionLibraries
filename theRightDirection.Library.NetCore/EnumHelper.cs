6158dba289cab921a8d54b764ea8ac007c9d0510
Mannus Etten <info@therightdirection.nl>
2024-05-26 10:10:15 +0200

    nieuwe functies vanuit Portal Genius

--- theRightDirection.Library.NetCore/EnumHelper.cs
+++ theRightDirection.Library.NetCore/EnumHelper.cs
@@ -1,99 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 
-namespace theRightDirection
+namespace theRightDirection;
+
+public static class EnumHelper
 {
-    public static class EnumHelper
+    /// <summary>
+    /// Parses the text value to enum value which is given by type T
+    /// </summary>
+    /// <typeparam name="T">the type of the enumeration</typeparam>
+    /// <param name="value">The value to parse</param>
+    /// <param name="ignoreCase">Ignore casing</param>
+    /// <returns>an enum value which belongs to T</returns>
+    public static T ParseTextToEnumValue<T>(string value, bool ignoreCase = true)
     {
-        /// <summary>
-        /// Parses the text value to enum value which is given by type T
-        /// </summary>
-        /// <typeparam name="T">the type of the enumeration</typeparam>
-        /// <param name="value">The value to parse</param>
-        /// <param name="ignoreCase">Ignore casing</param>
-        /// <returns>an enum value which belongs to T</returns>
-        public static T ParseTextToEnumValue<T>(string value, bool ignoreCase = true)
+        try
         {
-            try
-            {
-                var enumValue = Enum.Parse(typeof(T), value, ignoreCase);
-                var result = (T)enumValue;
-                return result;
-            }
-            catch (ArgumentException e)
-            {
-                throw new EnumHelperException(e.Message);
-            }
-        }
-
-        /// <summary>
-        /// Parses the text value to enum value which is given by type T in case of invalid value,
-        /// the out parameter will always become the first value in the enumeration
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="enumValueAsString">The enum value as string</param>
-        /// <param name="value">The value</param>
-        /// <param name="ignoreCase">Ignore casing</param>
-        /// <returns></returns>
-        public static bool TryParseTextToEnumValue<T>(string enumValueAsString, out T value, bool ignoreCase = true)
-        {
-            try
-            {
-                value = ParseTextToEnumValue<T>(enumValueAsString, ignoreCase);
-                return true;
-            }
-            catch
-            {
-                value = default(T);
-                return false;
-            }
-        }
-
-        public static IEnumerable<string> GetDescriptions<T>(bool descriptionsAsLowerCase = false)
-        {
-            List<string> result = new List<string>();
-            var items = GetValues<T>();
-            if (descriptionsAsLowerCase)
-            {
-                items.ForEach(x => result.Add(x.GetDescriptionAttribute().ToLowerInvariant()));
-            }
-            else
-            {
-                items.ForEach(x => result.Add(x.GetDescriptionAttribute()));
-            }
+            var enumValue = Enum.Parse(typeof(T), value, ignoreCase);
+            var result = (T)enumValue;
             return result;
         }
-
-        public static IEnumerable<T> GetValues<T>()
+        catch (ArgumentException e)
         {
-            return Enum.GetValues(typeof(T)).Cast<T>().ToList();
+            throw new EnumHelperException(e.Message);
         }
+    }
 
-        public static T ParseIntToEnumValue<T>(int enumValue)
+    /// <summary>
+    /// Parses the text value to enum value which is given by type T in case of invalid value,
+    /// the out parameter will always become the first value in the enumeration
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="enumValueAsString">The enum value as string</param>
+    /// <param name="value">The value</param>
+    /// <param name="ignoreCase">Ignore casing</param>
+    /// <returns></returns>
+    public static bool TryParseTextToEnumValue<T>(string enumValueAsString, out T value, bool ignoreCase = true)
+    {
+        try
         {
-            Array numericValues = Enum.GetValuesAsUnderlyingType(typeof(T));
-            Array textValues= Enum.GetValues(typeof(T));
-            var enumValues = new Dictionary<int, T>();
-            for (var i = 0; i < textValues.Length; i++)
-            {
-                var textValue = textValues.GetValue(i);
-                var numValue = numericValues.GetValue(i);
-                enumValues.Add((int)numValue, (T)textValue);
-            }
-            if (enumValues.ContainsKey(enumValue))
-            {
-                return enumValues[enumValue];
-            }
-            throw new EnumHelperException($"there is no value in the enum type {typeof(T)} with a value of {enumValue}");
+            value = ParseTextToEnumValue<T>(enumValueAsString, ignoreCase);
+            return true;
         }
-
-        public static T GetValueFromDescription<T>(string selectedWorkout)
+        catch
         {
-            Dictionary<string, T> values = new Dictionary<string, T>();
-            var items = GetValues<T>();
-            items.ForEach(x => values.Add(x.GetDescriptionAttribute(), x));
-            return values[selectedWorkout];
+            value = default(T);
+            return false;
+        }
+    }
+
+    public static IEnumerable<string> GetDescriptions<T>(bool descriptionsAsLowerCase = false)
+    {
+        List<string> result = new List<string>();
+        var items = GetValues<T>();
+        if (descriptionsAsLowerCase)
+        {
+            items.ForEach(x => result.Add(x.GetDescriptionAttribute().ToLowerInvariant()));
+        }
+        else
+        {
+            items.ForEach(x => result.Add(x.GetDescriptionAttribute()));
+        }
+        return result;
+    }
+
+    public static IEnumerable<T> GetValues<T>()
+    {
+        return Enum.GetValues(typeof(T)).Cast<T>().ToList();
+    }
+
+    public static T ParseIntToEnumValue<T>(int enumValue)
+    {
+        Array numericValues = Enum.GetValuesAsUnderlyingType(typeof(T));
+        Array textValues= Enum.GetValues(typeof(T));
+        var enumValues = new Dictionary<int, T>();
+        for (var i = 0; i < textValues.Length; i++)
+        {
+            var textValue = textValues.GetValue(i);
+            var numValue = numericValues.GetValue(i);
+            enumValues.Add((int)numValue, (T)textValue);
+        }
+        if (enumValues.ContainsKey(enumValue))
+        {
+            return enumValues[enumValue];
+        }
+        throw new EnumHelperException($"there is no value in the enum type {typeof(T)} with a value of {enumValue}");
+    }
+
+    public static T GetValueFromDescription<T>(string selectedWorkout)
+    {
+        Dictionary<string, T> values = new Dictionary<string, T>();
+        var items = GetValues<T>();
+        items.ForEach(x => values.Add(x.GetDescriptionAttribute(), x));
+        return values[selectedWorkout];
+    }
+
+    public static String? GetNameOrNull<TEnum>(this TEnum value)
+        where TEnum : struct, Enum
+    {
+        return EnumAttribCache<TEnum>.cachedNames.TryGetValue(value, out String? text) ? text : null;
+    }
+
+    public static String GetName<TEnum>(this TEnum value)
+        where TEnum : struct, Enum
+    {
+        return GetNameOrNull(value) ?? value.ToString();
+    }
+
+    private static class EnumAttribCache<TEnum>
+        where TEnum : struct, Enum
+    {
+        public static readonly ImmutableDictionary<TEnum, String> cachedNames = LoadNames();
+
+        private static ImmutableDictionary<TEnum, String> LoadNames()
+        {
+            return typeof(TEnum)
+                .GetTypeInfo()
+                .DeclaredFields
+                .Where(f => f.IsStatic && f.IsPublic && f.FieldType == typeof(TEnum))
+                .Select(f => (field: f, attrib: f.GetCustomAttribute<EnumMemberAttribute>()))
+                .Where(t => (t.attrib?.IsValueSetExplicitly ?? false) && !String.IsNullOrEmpty(t.attrib.Value))
+                .ToDictionary(
+                    keySelector: t => (TEnum)t.field.GetValue(obj: null)!,
+                    elementSelector: t => t.attrib!.Value!
+                )
+                .ToImmutableDictionary();
         }
     }
 }
--- theRightDirection.Library.NetCore/Extensions/Extensions.Uri.cs
+++ theRightDirection.Library.NetCore/Extensions/Extensions.Uri.cs
@@ -1,76 +1,140 @@
-﻿using System;
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using System.Web;
 
-namespace theRightDirection
+namespace theRightDirection;
+
+public static partial class Extensions
 {
-    public static partial class Extensions
+    /// <summary>
+    /// rewrite the uri, strip the values of queryparameters to a maximum of five characters<br/>
+    /// current supported query paramaters names:<br/>
+    /// - code<br/>
+    /// - code_verifier<br/>
+    /// - token<br/>
+    /// - passsword<br/>
+    /// - access_token<br/>
+    /// - refresh_token<br/>
+    /// - auth_token<br/>
+    /// - clientid
+    /// </summary>
+    public static string RemoveSensitiveDataFromUri(this Uri uri)
     {
-        /// <summary>
-        /// check if two URI's are equal, only applies to absolute uri's
-        /// </summary>
-        public static bool IsEqual(this Uri uri, Uri otherUri)
+        var sensitiveParameterNames = new List<string> { "code", "code_verifier", "token", "password", "access_token", "refresh_token", "auth_token", "clientid" };
+        if (uri.IsInvalidAbsoluteUri())
         {
-            if (!uri.IsAbsoluteUri || !otherUri.IsAbsoluteUri)
-            {
-                return false;
-            }
-            var uri1 = uri.AbsoluteUri.ToLowerInvariant();
-            var uri2 = otherUri.AbsoluteUri.ToLowerInvariant();
-            if (uri1.EndsWith("/"))
-            {
-                uri1 = uri1.RemoveSpecialCharacterAtTheEndFromString();
-            }
-            if (uri2.EndsWith("/"))
-            {
-                uri2 = uri1.RemoveSpecialCharacterAtTheEndFromString();
-            }
-            return uri1.Equals(uri2, StringComparison.Ordinal);
+            return string.Empty;
         }
+        var queryParameters = HttpUtility.ParseQueryString(uri.Query);
+        var parameters = new List<string>();
+        foreach (string parameter in queryParameters)
+        {
+            var value = queryParameters[parameter];
+            if (sensitiveParameterNames.Contains(parameter))
+            {
+                if (value.HasText() && value.Length > 5)
+                {
+                    value = value.Substring(0, 5);
+                }
+            }
+            parameters.Add($"{parameter}={value}");
+        }
+        return $"{uri.GetLeftPart(UriPartial.Path)}?{string.Join("&", parameters)}".Trim();
+    }
 
-        /// <summary>
-        /// open the uri in a new browser by process.start-call
-        /// </summary>
-        /// <param name="uri"></param>
-        public static void OpenInBrowser(this Uri uri)
+    /// <summary>
+    /// check if the uri != null and is absolute uri as well
+    /// </summary>
+    public static bool IsValidAbsoluteUri(this Uri uri)
+    {
+        if (uri == null)
         {
-            var url = uri.AbsoluteUri;
-            try
-            {
-                Process.Start(url);
-            }
-            catch
-            {
-                // hack because of this: https://github.com/dotnet/corefx/issues/10361
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                {
-                    Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                {
-                    Process.Start("xdg-open", url);
-                }
-                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-                {
-                    Process.Start("open", url);
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            return false;
         }
+        return uri.IsAbsoluteUri;
+    }
 
-        /// <summary>
-        /// rewrite a http-url to https
-        /// </summary>
-        public static Uri RewriteUriToSecureHttps(this Uri uri)
+    /// <summary>
+    /// check if the uri == null and is not absolute uri as well
+    /// </summary>
+    public static bool IsInvalidAbsoluteUri(this Uri uri)
+    {
+        if (uri == null)
         {
-            return new UriBuilder(uri)
-            {
-                Scheme = Uri.UriSchemeHttps,
-                Port = uri.IsDefaultPort ? -1 : uri.Port
-            }.Uri;
+            Log.Logger.Here().Debug($"uri is relative or null: '{uri}'");
+            return true;
         }
+        return !uri.IsAbsoluteUri;
+    }
+
+    /// <summary>
+    /// check if two URI's are equal, only applies to absolute uri's
+    /// </summary>
+    public static bool IsEqual(this Uri uri, Uri otherUri)
+    {
+        // TODO AnyDiff gebruiken
+        if (!uri.IsAbsoluteUri || !otherUri.IsAbsoluteUri)
+        {
+            return false;
+        }
+        var uri1 = uri.AbsoluteUri.ToLowerInvariant();
+        var uri2 = otherUri.AbsoluteUri.ToLowerInvariant();
+        if (uri1.EndsWith("/"))
+        {
+            uri1 = uri1.RemoveSpecialCharacterAtTheEndFromString();
+        }
+        if (uri2.EndsWith("/"))
+        {
+            uri2 = uri1.RemoveSpecialCharacterAtTheEndFromString();
+        }
+        return uri1.Equals(uri2, StringComparison.Ordinal);
+    }
+
+    /// <summary>
+    /// open the uri in a new browser by process.start-call
+    /// </summary>
+    /// <param name="uri"></param>
+    public static void OpenInBrowser(this Uri uri)
+    {
+        var url = uri.AbsoluteUri;
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+
+    /// <summary>
+    /// rewrite a http-url to https
+    /// </summary>
+    public static Uri RewriteUriToSecureHttps(this Uri uri)
+    {
+        return new UriBuilder(uri)
+        {
+            Scheme = Uri.UriSchemeHttps,
+            Port = uri.IsDefaultPort ? -1 : uri.Port
+        }.Uri;
     }
 }
