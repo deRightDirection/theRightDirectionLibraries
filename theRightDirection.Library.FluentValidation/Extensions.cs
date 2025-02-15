e3ab45fb4280a1bf730dfbe75552828a843e5d85
Mannus Etten <info@therightdirection.nl>
2025-02-15 09:09:39 +0100

    fluent validation toevoeging

---
+++ theRightDirection.Library.FluentValidation/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System.Text;
+
+namespace theRightDirection.Library.FluentValidation;
+
+public static partial class Extensions
+{
+    /// <summary>
+    /// small helper method which combines all error messages with the stringbuilder and does appendline, helping
+    /// with formatting the error messages in log files
+    /// </summary>
+    public static string CombineErrorsToString(this ValidationResult validationResult)
+    {
+        var sb = new StringBuilder();
+        validationResult.Errors.ForEach(x => sb.AppendLine(x.ErrorMessage));
+        return sb.ToString().Trim();
+    }
+}
---
+++ theRightDirection.Tests/Library.FluentValidation/ExtensionsTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using FluentValidation;
+using theRightDirection.Library.FluentValidation;
+
+namespace theRightDirection.Tests.Library.FluentValidation;
+
+public class ExtensionsTest
+{
+    [Fact]
+    public void CombineErrorMessages()
+    {
+        var sut = new TestObject();
+        sut.Name = string.Empty;
+        var validator = new TestObjectValidator();
+        var results = validator.Validate(sut);
+        var result = results.CombineErrorsToString();
+        result.Should().Be("te kort\r\nniet leeg");
+    }
+}
+
+internal class TestObject
+{
+    public string Name { get; set; }
+}
+
+internal class TestObjectValidator : AbstractValidator<TestObject>
+{
+    public TestObjectValidator()
+    {
+        RuleFor(x => x.Name)
+            .MinimumLength(3)
+            .WithMessage("te kort");
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("niet leeg");
+    }
+}
