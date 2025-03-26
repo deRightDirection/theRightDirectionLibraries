bec0bbcb155b21c1ef40b4f66c0d4fcefb0664df
Mannus Etten <info@therightdirection.nl>
2025-03-26 06:24:48 +0100

    middleware library toegevoegd

---
+++ theRightDirection.AspNet.Middleware/GeoBlock/GeoBlockMiddleWareConfiguration.cs
@@ -0,0 +1,58 @@
+﻿using MaxMind.Db;
+using Serilog;
+using System.Net;
+
+namespace theRightDirection.AspNet.Middleware.GeoBlock;
+
+public class GeoBlockMiddleWareConfiguration
+{
+    private readonly Reader _reader;
+    private readonly string[] _isoCountryNames;
+
+    public GeoBlockMiddleWareConfiguration(string maxMindDatabaseFilePath, List<string> isoCountryNames = null)
+    {
+        if (File.Exists(maxMindDatabaseFilePath))
+        {
+            _reader = new Reader(maxMindDatabaseFilePath, FileAccessMode.Memory);
+        }
+        // NL is Nederland
+        // DE is Duitsland
+        if (isoCountryNames == null)
+        {
+            isoCountryNames = new List<string> { "NL", "DE" };
+        }
+        _isoCountryNames = isoCountryNames.ToArray();
+    }
+
+    public bool IPIsFromAllowedCountry(IPAddress ip)
+    {
+        var data = Lookup(ip);
+        var countryCode = GetCountryCode(data);
+        Log.Logger.Here().Debug($"ISO country code for IP: {countryCode}");
+        if (countryCode.HasNoText() || !_isoCountryNames.Contains(countryCode))
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private string GetCountryCode(Dictionary<string, object> data)
+    {
+        if (data != null)
+        {
+            var cnty = data["country"] as Dictionary<string, object>;
+            Log.Logger.Here().Debug((string)cnty["iso_code"]);
+            return (string)cnty["iso_code"];
+        }
+        return string.Empty;
+    }
+
+    private Dictionary<string, object> Lookup(IPAddress ip)
+    {
+        if (_reader == null)
+        {
+            return new Dictionary<string, object>();
+        }
+        return _reader?.Find<Dictionary<string, object>>(ip);
+    }
+}
---
+++ theRightDirection.AspNet.Middleware/GeoBlock/GeoBlockMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Caching.Memory;
+using Serilog;
+
+namespace theRightDirection.AspNet.Middleware.GeoBlock;
+
+public class GeoBlockMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly GeoBlockMiddleWareConfiguration _geo;
+    private readonly IMemoryCache _cache;
+
+    public GeoBlockMiddleware(RequestDelegate next, GeoBlockMiddleWareConfiguration geo, IMemoryCache cache)
+    {
+        _next = next;
+        _geo = geo;
+        _cache = cache;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        var ipAddres = context.Connection.RemoteIpAddress?.ToString();
+        Log.Logger.Here().Debug(context.Connection.RemoteIpAddress.ToString());
+        var ipAllowed = _cache.GetOrCreate(ipAddres, x => _geo.IPIsFromAllowedCountry(context.Connection.RemoteIpAddress));
+        if (ipAllowed)
+        {
+            await _next.Invoke(context);
+        }
+    }
+}
